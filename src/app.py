7267597e0a6a1b2d4d12149f84df9f021be984f8
Jeffrey Boschman <jeffreyboschman@gmail.com>
2024-02-17 13:12:03 +0900

    Add ability to edit system prompt from gradio ui

--- src/app.py
+++ src/app.py
@@ -2,7 +2,20 @@ import asyncio
 
 import gradio as gr
 
-from agents.basic import greet, ask_with_context
+from agents.basic import ask_with_context
+
+_SYSTEM_PROMPT = """
+    Please act as an expert in social media, marketing, and psychology 
+    who is hired to be our social media content assistant.
+
+    We are a Canadian couple on YouTube and Instagram who 
+    mainly make posts about our life in Tokyo, Japan. 
+
+    Our vibe is cozy, charming, polite, casual, light, bright,
+    clean, relatable, humble, genuine, and Ghibli-esque.
+
+    Please help us develop content by following the user instructions or answering questions.
+    """
 
 
 def main():
@@ -17,11 +30,18 @@ def main():
         with gr.Row():
             with gr.Column(scale=2):
                 inp = gr.Textbox(placeholder="Type in your question.")
-                input_button = gr.Button(value="Get Response")
+                get_response_btn = gr.Button(value="Get Response")
+
+                default_prompt = gr.Textbox(
+                    label="System Prompt",
+                    value=_SYSTEM_PROMPT,
+                    interactive=True,
+                    elem_id="system-prompt",
+                )
             with gr.Column(scale=3):
                 out = gr.Textbox()
 
-        input_button.click(ask_with_context, inp, out)
+        get_response_btn.click(ask_with_context, [inp, default_prompt], out)
 
     demo.launch()
 
